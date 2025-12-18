224093f44f2a21b33b90fb9005147c92a05c4363
Artur Senichkin <asylumfight28@gmail.com>
2025-12-18 18:53:38 +0200

    add day 4 solution

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use crate::day1::{day1};
 use crate::day2::{day2};
 use crate::day3::{day3};
+use crate::day4::{day4};
 
 pub mod day1;
 pub mod day2;
 pub mod day3;
+pub mod day4;
 
 fn main() {
-    day1();
-    day2();
-    day3();
+    // day1();
+    // day2();
+    // day3();
+    day4();
 }
