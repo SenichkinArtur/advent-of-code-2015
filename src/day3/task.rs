9668a345032cfdf71518c0595754814ed0f26ed8
Artur Senichkin <asylumfight28@gmail.com>
2025-12-18 16:14:52 +0200

    day 3 refactoring: update types, variable names; rewrite vector to HashSet

--- src/day3/task.rs
+++ src/day3/task.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::read_to_string;
 
 const NORTH: char = '^';
@@ -5,51 +6,69 @@ const SOUTH: char = 'v';
 const EAST: char = '>';
 const WEST: char = '<';
 
+type CoordsArray = (i32, i32);
+
 pub fn day3() {
-    get_number_visited_houses();
-    get_number_visited_houses_robo();
+    let input: String = read_to_string("src/day3/input.txt").unwrap();
+
+    let part1: usize = get_number_visited_houses(&input);
+    let part2: usize = get_number_visited_houses_robo(&input);
+
+    println!("Day 3 part 1 answer is {}", part1);
+    println!("Day 3 part 2 answer is {}", part2);
 }
 
-fn get_number_visited_houses() {
-    let input: String = read_to_string("src/day3/input.txt").unwrap();
-    let mut current_pos: [i32; 2] = [0, 0]; // [x, y]
-    let mut visited_houses_coords: Vec<[i32; 2]> = vec![current_pos];
+fn get_number_visited_houses(input: &str) -> usize {
+    let mut current_pos: CoordsArray = (0, 0); // [x, y]
+    let mut visited_houses_coords: HashSet<CoordsArray> = HashSet::new();
+    visited_houses_coords.insert(current_pos);
 
-    for char in input.chars() {
-        set_visited_coords(char, &mut visited_houses_coords, &mut current_pos);
+    for direction in input.trim().chars() {
+        set_visited_coords(direction, &mut visited_houses_coords, &mut current_pos);
     }
 
-    println!("Day 3 part 1 answer is {}", visited_houses_coords.len());
+    return visited_houses_coords.len();
 }
 
-fn get_number_visited_houses_robo() {
-    let input: String = read_to_string("src/day3/input.txt").unwrap();
-    let mut current_pos_santa: [i32; 2] = [0, 0]; // [x, y]
-    let mut current_pos_robo_santa: [i32; 2] = [0, 0]; // [x, y]
-    let mut visited_houses_coords: Vec<[i32; 2]> = vec![current_pos_santa];
+fn get_number_visited_houses_robo(input: &str) -> usize {
+    let mut current_pos_santa: CoordsArray = (0, 0); // [x, y]
+    let mut current_pos_robo_santa: CoordsArray = (0, 0); // [x, y]
+    let mut visited_houses_coords: HashSet<CoordsArray> = HashSet::new();
+    visited_houses_coords.insert(current_pos_santa);
+    visited_houses_coords.insert(current_pos_robo_santa);
 
-    for (index, char) in input.char_indices() {
+    for (index, direction) in input.trim().chars().enumerate() {
         let is_santa: bool = index % 2 == 0;
 
         if is_santa {
-            set_visited_coords(char, &mut visited_houses_coords, &mut current_pos_santa);
+            set_visited_coords(
+                direction,
+                &mut visited_houses_coords,
+                &mut current_pos_santa,
+            );
         } else {
-            set_visited_coords(char, &mut visited_houses_coords, &mut current_pos_robo_santa);
+            set_visited_coords(
+                direction,
+                &mut visited_houses_coords,
+                &mut current_pos_robo_santa,
+            );
         }
     }
 
-    println!("Day 3 part 2 answer is {}", visited_houses_coords.len());
+    return visited_houses_coords.len();
 }
 
-fn set_visited_coords(char: char, visited_houses_coords: &mut Vec<[i32; 2]>, current_pos: &mut [i32; 2]) {
-    match char {
-        NORTH => current_pos[1] += 1,
-        SOUTH => current_pos[1] -= 1,
-        EAST => current_pos[0] += 1,
-        WEST => current_pos[0] -= 1,
+fn set_visited_coords(
+    direction: char,
+    visited_houses_coords: &mut HashSet<CoordsArray>,
+    current_pos: &mut CoordsArray,
+) {
+    match direction {
+        NORTH => current_pos.1 += 1,
+        SOUTH => current_pos.1 -= 1,
+        EAST => current_pos.0 += 1,
+        WEST => current_pos.0 -= 1,
         _ => {}
     }
-    if !visited_houses_coords.contains(current_pos) {
-        visited_houses_coords.push(*current_pos);
-    }
+    visited_houses_coords.insert(*current_pos);
 }
