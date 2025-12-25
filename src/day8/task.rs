eb77286577cbb9556caa9906df55d8a34b7eb5c0
Artur Senichkin <asylumfight28@gmail.com>
2025-12-25 17:38:44 +0200

    add day 8 part 2 solution; remove debug logs

--- src/day8/task.rs
+++ src/day8/task.rs
@@ -5,31 +5,24 @@ const DOUBLE_QUOTES_LENGTH: usize = 2;
 
 pub fn day8() {
     let input: Vec<String> = get_lines("src/day8/input.txt");
-    let part1 = get_space(&input);
+    let (part1, part2)  = get_space(&input);
 
     println!("Day 8 part 1 answer is {}", part1);
+    println!("Day 8 part 2 answer is {}", part2);
 }
 
-fn get_space(lines: &[String]) -> usize {
+fn get_space(lines: &[String]) -> (usize, usize) {
     let mut code_len: usize = 0;
     let mut memory_len: usize = 0;
+    let mut escaped_len: usize = 0;
 
     for line in lines {
         code_len += line.len();
         memory_len += unescape(line).unwrap().chars().count() - DOUBLE_QUOTES_LENGTH;
-        println!(
-            "line: {}, unescaped: {}, code_len: {}, memory_len: {}",
-            line,
-            unescape(line).unwrap(),
-            code_len,
-            memory_len
-        );
+        escaped_len += line.escape_default().count() + DOUBLE_QUOTES_LENGTH;
     }
 
-    println!("chars_code_number: {}", code_len);
-    println!("chars_memory_number: {}", memory_len);
-
-    code_len - memory_len
+    (code_len - memory_len, escaped_len - code_len)
 }
 
 fn get_lines(path: &str) -> Vec<String> {
