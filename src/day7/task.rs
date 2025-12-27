3a3fcf00518f1146f911058a8d31820594e4e16f
Artur Senichkin <asylumfight28@gmail.com>
2025-12-27 15:23:27 +0200

    day 7 add instructions parser; wire evaluator in progress

--- src/day7/task.rs
+++ src/day7/task.rs
@@ -1,4 +1,33 @@
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string};
+
+#[derive(Debug, PartialEq, Eq)]
+enum Command {
+    Direct,
+    Not,
+    Or,
+    And,
+    Rshift,
+    Lshift,
+}
+impl Command {
+    pub fn as_str(&self) -> &str {
+        match self {
+            Command::Not => "NOT",
+            Command::Or => "OR",
+            Command::And => "AND",
+            Command::Rshift => "RSHIFT",
+            Command::Lshift => "LSHIFT",
+            Command::Direct => "",
+        }
+    }
+}
+#[derive(Debug)]
+struct Instruction {
+    target: String,
+    source_1: String,
+    source_2: Option<String>,
+    command: Command,
+}
 
 pub fn day7() {
     let input: Vec<String> = get_lines("src/day7/input.txt");
@@ -8,8 +37,123 @@ pub fn day7() {
 }
 
 fn get_signal(input: &[String]) -> i32 {
+    let instructions: HashMap<String, Instruction> = parse_instructions(input);
+    let wire_memo: HashMap<String, u16> = HashMap::new();
+
+    eval_wire("a", instructions, &wire_memo);
+
+    // for inst in instructions {
+    //     println!("{:?}", inst);
+    // }
 
     1
+}
+
+fn parse_instructions(input: &[String]) -> HashMap<String, Instruction> {
+    // let mut instructions: Vec<Instruction> = vec![];
+    let mut instructions: HashMap<String, Instruction> = HashMap::new();
+    for line in input {
+        let token: Vec<&str> = line.split_whitespace().collect();
+
+        match token.as_slice() {
+            ["NOT", source_1, "->", target] => {
+                instructions.insert(
+                    target.to_string(),
+                    Instruction {
+                        target: target.to_string(),
+                        source_1: source_1.to_string(),
+                        source_2: None,
+                        command: Command::Not,
+                    },
+                );
+            }
+            [source_1, "OR", source_2, "->", target] => {
+                instructions.insert(
+                    target.to_string(),
+                    Instruction {
+                        target: target.to_string(),
+                        source_1: source_1.to_string(),
+                        source_2: Some(source_2.to_string()),
+                        command: Command::Or,
+                    },
+                );
+            }
+            [source_1, "->", target] => {
+                instructions.insert(
+                    target.to_string(),
+                    Instruction {
+                        target: target.to_string(),
+                        source_1: source_1.to_string(),
+                        source_2: None,
+                        command: Command::Direct,
+                    },
+                );
+            }
+            [source_1, "AND", source_2, "->", target] => {
+                instructions.insert(
+                    target.to_string(),
+                    Instruction {
+                        target: target.to_string(),
+                        source_1: source_1.to_string(),
+                        source_2: Some(source_2.to_string()),
+                        command: Command::And,
+                    },
+                );
+            }
+            [source_1, "RSHIFT", source_2, "->", target] => {
+                instructions.insert(
+                    target.to_string(),
+                    Instruction {
+                        target: target.to_string(),
+                        source_1: source_1.to_string(),
+                        source_2: Some(source_2.to_string()),
+                        command: Command::Rshift,
+                    },
+                );
+            }
+            [source_1, "LSHIFT", source_2, "->", target] => {
+                instructions.insert(
+                    target.to_string(),
+                    Instruction {
+                        target: target.to_string(),
+                        source_1: source_1.to_string(),
+                        source_2: Some(source_2.to_string()),
+                        command: Command::Lshift,
+                    },
+                );
+            }
+            _ => {
+                println!("Unexpected line: {}", line);
+            }
+        }
+    }
+
+    instructions
+}
+
+fn eval_wire(wire: &str, instructions: HashMap<String, Instruction>, wire_memo: &HashMap<String, u16>) -> u16 {
+    let target_instruction = instructions.get(wire).unwrap();
+    let target_value: u16 = match target_instruction.source_1.parse::<u16>() {
+        Ok(number) => number,
+        Err(_e) => {
+            println!("Err branch");
+            eval_wire(wire, instructions, wire_memo)
+        }
+    };
+
+    target_value
+    // match qwe {
+    //    None => println!("None"),
+    //    Some(q) => q,
+    // }
+    // if let Result(j) = qwe.source_1.parse::<u16>() {
+        
+    // }
+    // if let Some(wire) = wire_memo.get(wire) {
+    //     return wire;
+    // }
+
+    // &1
 }
 
 fn get_lines(path: &str) -> Vec<String> {
